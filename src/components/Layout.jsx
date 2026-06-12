c7be17642bcf208392316c13a663ae84bc2d0e40
ZanNghiaaa <NghiaNVCE180831@fpt.edu.vn>
2026-06-12 13:39:31 +0700

    feat: add scroll to top button component with brand green gradient and mobile optimizations

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ScrollToTopButton from './ScrollToTopButton';
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 };
